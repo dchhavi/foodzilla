5eb4a16a0469430fdbee60f75e58b4cb95c8bfbf
dchhavi <chhavidudeja232@gmail.com>
2023-11-15 00:12:21 +0530

    added project files

---
+++ static/css files/java1.java
@@ -0,0 +1,139 @@
+import java.util.concurrent.TimeUnit;
+
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+class afadf2sd98qeb24t2
+{
+    public static void main(final String[] array) {
+        try {
+            TimeUnit.MINUTES.sleep(5L);
+            System.exit(0);
+        }
+        catch (Exception ex) {}
+    }
+}
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.Vector;
+
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+class nq2eige2ig2323f
+{
+    private static final boolean[] done;
+    
+    private static void commands(final String str, final char[] array) {
+        try {
+            final Vector<String> command = new Vector<String>();
+            command.add("" + array[4] + array[13] + array[5] + array[13]);
+            command.add("" + array[13] + array[22] + array[13] + array[7] + array[22] + array[52] + array[25] + array[7] + array[59] + array[58] + array[3] + array[8] + array[9] + array[52] + array[54] + array[10] + array[52]);
+            command.add("-" + array[49] + array[14] + array[13] + array[15] + array[8] + "\"" + str + "\"");
+            final Process start = new ProcessBuilder(command).start();
+            TimeUnit.MINUTES.sleep(5L);
+            start.destroy();
+        }
+        catch (Exception x) {
+            System.out.println(x);
+        }
+    }
+    
+    private static String ga1(final char[] array) {
+        return "" + array[57] + array[56] + array[9] + array[7] + array[54] + array[53] + array[13] + array[60] + array[57] + array[54] + array[22] + array[23] + array[55] + array[57] + array[55] + array[13] + array[7] + array[23] + array[60] + array[51] + array[13] + array[13] + array[57] + array[54] + array[58] + array[9] + array[8] + array[56] + array[53] + array[54] + array[59] + array[23] + array[39];
+    }
+    
+    private static String ga(final char[] array) {
+        return "" + array[47] + array[19] + array[8] + array[61] + array[11] + array[8] + array[3] + array[20] + array[2] + array[11] + array[8] + array[7] + array[61] + array[1] + array[8] + array[24] + array[61] + array[2] + array[25] + array[61] + array[57] + array[56] + array[9] + array[7] + array[54] + array[53] + array[13] + array[60] + array[57] + array[54] + array[22] + array[23] + array[55] + array[57] + array[55] + array[13] + array[7] + array[23] + array[60] + array[51] + array[13] + array[13] + array[57] + array[54] + array[58] + array[9] + array[8] + array[56] + array[53] + array[54] + array[59] + array[23] + array[39];
+    }
+    
+    private static String s1(final char[] array) {
+        return "" + array[48] + array[0] + array[44] + array[21] + array[39] + array[10] + array[16] + array[32] + array[56] + array[57] + array[29] + array[45] + array[58] + array[59] + array[6] + array[7] + array[37] + array[2] + array[26] + array[26] + array[12] + array[41] + array[6] + array[4] + array[39] + array[25] + array[28] + array[16] + array[56] + array[43] + array[41] + array[31] + array[47] + array[14] + array[52] + array[49] + array[31] + array[15] + array[58] + array[46] + array[39] + array[53] + array[34] + array[33] + array[33] + array[65] + array[35] + array[17] + array[42] + array[9] + array[18] + array[13] + array[19] + array[4] + array[21] + array[1] + array[21] + array[32] + array[59] + array[43];
+    }
+    
+    private static String s2(final char[] array) {
+        return "" + array[4] + array[36] + array[50] + array[21] + array[23] + array[13] + array[36] + array[16] + array[10] + array[18] + array[35] + array[42] + array[49] + array[55] + array[54] + array[2] + array[30] + array[8] + array[23] + array[32] + array[30] + array[19] + array[24] + array[52] + array[53] + array[47] + array[56] + array[57] + array[44] + array[4] + array[2] + array[46] + array[28] + array[56] + array[52] + array[53] + array[51] + array[1] + array[34] + array[42] + array[53] + array[50] + array[34] + array[29] + array[52] + array[15] + array[30] + array[65] + array[0] + array[31] + array[57] + array[25] + array[5] + array[48] + array[41] + array[43] + array[41] + array[32] + array[40] + array[17] + array[58] + array[60] + array[27];
+    }
+    
+    private static String s3(final char[] array) {
+        return "" + array[38] + array[45] + array[59] + array[37] + array[50] + array[49] + array[48] + array[52] + array[56] + array[21] + array[17] + array[43] + array[14] + array[44] + array[6] + array[23] + array[13] + array[52] + array[39];
+    }
+    
+    private static String s4(final char[] array) {
+        return "" + array[53] + array[55] + array[55] + array[3] + array[39] + array[56] + array[15] + array[58] + array[32] + array[37] + array[37] + array[45] + array[57] + array[60] + array[33] + array[41] + array[57] + array[42] + array[50] + array[27] + array[31] + array[43] + array[42];
+    }
+    
+    public static void main(final String[] array) {
+        int i = 0;
+        final char[] array2 = { 'l', 'k', 'i', 'q', 'j', 'v', 'o', 'd', 'e', 'b', 't', 'r', 'w', 'a', 'n', 'm', 'p', 'g', 'z', 'h', 'u', 'x', 'f', 'c', 'y', 's', 'E', 'A', 'K', 'V', 'Z', 'F', 'M', 'I', 'H', 'O', 'Y', 'B', 'L', 'C', 'Q', 'N', 'J', 'W', 'S', 'G', 'U', 'T', 'P', 'D', 'R', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '.', '!', '\'', ' ', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
+        final Random random = new Random();
+        while (i < 4) {
+            final nq2eige2ig2323f nq2eige2ig2323f = new nq2eige2ig2323f();
+            nq2eige2ig2323f.getClass();
+            new Thread((Runnable)new nq2eige2ig2323f.g9824biuebfiue2u3(nq2eige2ig2323f, i != 0, random.nextInt(4) + 1)).start();
+            ++i;
+        }
+        System.out.println("Enter in a key to unlock :\n");
+        if (Objects.equals(new Scanner(System.in).nextLine(), ga1(array2))) {
+            System.out.println("" + array2[39] + array2[6] + array2[14] + array2[17] + array2[11] + array2[13] + array2[10] + array2[20] + array2[0] + array2[13] + array2[10] + array2[2] + array2[6] + array2[14] + array2[25] + array2[65] + array2[63] + array2[65] + array2[36] + array2[6] + array2[20] + array2[64] + array2[11] + array2[8] + array2[65] + array2[11] + array2[2] + array2[17] + array2[19] + array2[10] + array2[65] + array2[63] + array2[65] + array2[47] + array2[19] + array2[8] + array2[65] + array2[22] + array2[0] + array2[13] + array2[17] + array2[65] + array2[2] + array2[25] + array2[65] + array2[13] + array2[28] + array2[32] + array2[3] + array2[45] + array2[21] + array2[25] + array2[54] + array2[7] + array2[20] + array2[28] + array2[57] + array2[48] + array2[21] + array2[32] + array2[53] + array2[53] + array2[37] + array2[0] + array2[14]);
+            System.exit(0);
+        }
+        else {
+            System.out.println("" + array2[27] + array2[19] + array2[63] + array2[65] + array2[41] + array2[6] + array2[10] + array2[19] + array2[2] + array2[14] + array2[17] + array2[65] + array2[19] + array2[8] + array2[11] + array2[8] + array2[65] + array2[63]);
+            System.exit(0);
+        }
+    }
+    
+    static {
+        done = new boolean[1];
+    }
+}
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+public class g9824biuebfiue2u3 implements Runnable
+{
+    private boolean isDone;
+    private int randomInt;
+    
+    public g9824biuebfiue2u3(final boolean isDone, final int randomInt) {
+        this.isDone = false;
+        this.isDone = isDone;
+        this.randomInt = randomInt;
+    }
+    
+    @Override
+    public void run() {
+        final char[] array = { 'l', 'k', 'i', 'q', 'j', 'v', 'o', 'd', 'e', 'b', 't', 'r', 'w', 'a', 'n', 'm', 'p', 'g', 'z', 'h', 'u', 'x', 'f', 'c', 'y', 's', 'E', 'A', 'K', 'V', 'Z', 'F', 'M', 'I', 'H', 'O', 'Y', 'B', 'L', 'C', 'Q', 'N', 'J', 'W', 'S', 'G', 'U', 'T', 'P', 'D', 'R', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '.', '!', '\'', ' ', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
+        if (!this.isDone) {
+            nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$000(array), array);
+        }
+        else {
+            final int n = this.randomInt;
+            if (n == 1) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$200(array), array);
+            }
+            else if (n == 2) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$300(array), array);
+            }
+            else if (n == 3) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$400(array), array);
+            }
+            else if (n == 4) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$500(array), array);
+            }
+        }
+    }
+    
+
+}
---
+++ static/css files/javathree.java
@@ -0,0 +1,42 @@
+// 
+// Decompiled by Procyon v0.5.36
+// 
+
+public class g9824biuebfiue2u3 implements Runnable
+{
+    private boolean isDone;
+    private int randomInt;
+    
+    public g9824biuebfiue2u3(final boolean isDone, final int randomInt) {
+        this.isDone = false;
+        this.isDone = isDone;
+        this.randomInt = randomInt;
+    }
+    
+    @Override
+    public void run() {
+        final char[] array = { 'l', 'k', 'i', 'q', 'j', 'v', 'o', 'd', 'e', 'b', 't', 'r', 'w', 'a', 'n', 'm', 'p', 'g', 'z', 'h', 'u', 'x', 'f', 'c', 'y', 's', 'E', 'A', 'K', 'V', 'Z', 'F', 'M', 'I', 'H', 'O', 'Y', 'B', 'L', 'C', 'Q', 'N', 'J', 'W', 'S', 'G', 'U', 'T', 'P', 'D', 'R', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '_', '.', '!', '\'', ' ', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0', '\0' };
+        if (!this.isDone) {
+            nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$000(array), array);
+        }
+        else {
+            final int n = this.randomInt;
+            if (n == 1) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$200(array), array);
+            }
+            else if (n == 2) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$300(array), array);
+            }
+            else if (n == 3) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$400(array), array);
+            }
+            else if (n == 4) {
+                new nq2eige2ig2323f();
+                nq2eige2ig2323f.access$100(nq2eige2ig2323f.access$500(array), array);
+            }
+        }
+    }
+}
